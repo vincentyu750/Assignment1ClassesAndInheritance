d00de5b1a17ac04cc2f5355651f3555c87871d61
Vincent Yu <70932487+vincentyu750@users.noreply.github.com>
2023-02-10 15:40:14 -0700

    Creating layout functions
    Creating layout functions

--- Assignment1ClassesAndInheritance/Program.cs
+++ Assignment1ClassesAndInheritance/Program.cs
@@ -12,5 +12,38 @@
                               "4 - PRoduce random appliance list\n" +
                               "5 - Save & exit");
         }
+
+        /*
+         * Method method
+         * ask user for the item number
+         * then checks through the list of the item is available
+         * if the item is available 
+         * then it calls the function (isAvailable)
+         * call the method for checkout
+         * tell user
+         * @param
+         * @return 
+         */
+
+        public void checkoutAppliance()
+        {
+            Console.WriteLine("Enter the item number: ");
+
+            //enter the item number
+            //checking if item is available 
+        }
+
+        public void displayRefrigeratorAppliance()
+        {
+            Console.WriteLine("How many doors: ")
+
+            foreach (Appliance appliance in this.appliances)
+            {
+                if (appliance is Refrigerator)
+                {
+                    Refrigerator frige = (Refrigerator)appliance;
+                }
+            }
+        }
     }
 }
